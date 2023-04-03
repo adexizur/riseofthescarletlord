0a5aa292de58867857eabfe1b920f6edd6423208
adexizur_ <74697591+adexizur@users.noreply.github.com>
2023-04-03 18:49:54 +0300

    Update nsup.js

--- js/nsup.js
+++ js/nsup.js
@@ -1,4 +1,9 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-  var notification = document.getElementById('notification');
-  notification.style.display = 'block';
-}
+window.addEventListener('resize', function() {
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    var notification = document.getElementById('notification');
+    notification.style.display = 'block';
+  } else {
+    var notification = document.getElementById('notification');
+    notification.style.display = 'none';
+  }
+});
